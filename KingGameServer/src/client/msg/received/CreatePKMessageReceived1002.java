722a9145cd4ff12f93a511e7e2b981aa428bcc85
LouiLam <78117253@qq.com>
2014-07-06 14:29:32 +0800

    最新公网ip 117.18.65.102
    最新公网ip 117.18.65.102

--- KingGameServer/src/client/msg/received/CreatePKMessageReceived1002.java
+++ KingGameServer/src/client/msg/received/CreatePKMessageReceived1002.java
@@ -103,7 +103,7 @@ public class CreatePKMessageReceived1002 extends SocketMessageReceived {
 		CloseableHttpClient httpClient = HttpClients.createDefault();
 		HttpPost httpPost = new HttpPost(
 				
-				"http://www.hexcm.com/yxlm/member/fight_add.php");
+				"http://117.18.65.102/yxlm/member/fight_add.php");
 
 		List<NameValuePair> nvps = new ArrayList<NameValuePair>();
 		nvps.add(new BasicNameValuePair("action", "dopost"));
